3eb5fbf2cf00b655b34691c31a9ade6cd36bae23
rysyx <git@rysyx.dev>
2020-08-29 13:47:29 -0400

    commit

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 apt install vim zsh tmux htop git curl -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/RysYx/.files.git
+cd .files
 mv .tmux.conf ../.tmux.conf
 mv .vimrc ../.vimrc
 mv .zshrc ../.zshrc
