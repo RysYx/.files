40e725844ca4c7aa90f53267d5b3698775114e09
rysyx <git@rysyx.dev>
2020-08-29 14:07:16 -0400

    commit

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+su root
 apt install vim zsh tmux htop git curl -y
 cd ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -8,3 +9,5 @@ cd .files
 mv .tmux.conf ../.tmux.conf
 mv .vimrc ../.vimrc
 mv .zshrc ../.zshrc
+cd ..
+rm -fr .files
